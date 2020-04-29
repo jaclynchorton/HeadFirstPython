45a942b4d095fb73bb35117f7b53dfb0480c9e57
jaclynchorton <jaclynchorton@gmail.com>
2020-04-29 13:35:04 -0700

    Commit for DBcm adding try to __end__

--- DBcm.py
+++ DBcm.py
@@ -4,6 +4,12 @@ import mysql.connector
 class ConnectionError(Exception):
     pass
 
+class CredentialsError(Exception):
+    pass
+
+class SQLError(Exception):
+    pass
+
 class UseDatabase:
     #The optional "None" annotation confirms that this method has no return value and the colon terminates the "def" line
     def __init__(self, config: dict) -> None: #Dunder "init" accepts a single dictionary, which we're calling "config
@@ -17,10 +23,17 @@ class UseDatabase:
             return self.cursor
         except mysql.connector.errors.InterfaceError as err:
             raise ConnectionError(err)
+        except mysql.connector.errors.ProgrammingError as err:
+            raise CredentialsError(err)
         
 
     def __exit__(self, exc_type, exc_value, exc_trace) -> None: #This annotation confirms that this method has no return value; such annotations are optional but are good practice.
         self.conn.commit()
         self.cursor.close()
         self.conn.close() #The previously used attributes are used to commit unsaved data, as well as close the cursor and connection. 
+        if exc_type is mysql.connector.errors.ProgrammingError:
+            raise SQLError(exc_value)
+        elif exc_type:
+            raise exc_type(exc_value)
+
         
